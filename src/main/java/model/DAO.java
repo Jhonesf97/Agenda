ce3b2106b65289fafc9346be9d124a6d011ab524
jhones <jhones.f97@gmail.com>
2022-11-17 21:32:46 -0300

    subindo o projeto

---
+++ src/main/java/model/DAO.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Iterator;
+
+public class DAO {
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+
+	private String user = "root";
+	private String password = "@dmin12#$";
+
+	private Connection conectar() {
+		Connection con = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+
+		}
+	}
+}
---
+++ src/main/webapp/agenda.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Agenda de contatos</title>
+<link rel="icon" href="imagens/phone.png">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Agenda de Contato</h1>
+	<a href="novo.html" class="Botao1">Novo Contato</a>
+</body>
+</html>
